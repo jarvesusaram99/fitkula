8b00751eb4e4318cdb464eb3eacc8109663588cd
Jarves Usaram <usaramwasi99@gmail.com>
2023-06-14 17:53:52 +0530

    14 june

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-	// url: "mongodb://localhost:27017/fitkula_db"
-    url:process.env.LIVE_DATABASE_URL
+	url: "mongodb://localhost:27017/fitkula_db"
+    // url:process.env.LIVE_DATABASE_URL
   };
 
   
